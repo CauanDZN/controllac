f1a3ee5f5e73159f726d025896f2a3277cec0254
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-09-12 22:06:56 -0300

    :game_die: Configuring Firebase

---
+++ App.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native"
+
+import firestore from '@react-native-firebase/firestore';
+
+export default function App(){
+
+  return (
+    <>
+      <View>
+        <Text>Controllac</Text>
+      </View>
+    </>
+  );
+}
