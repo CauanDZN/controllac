a13af9ce561a0b6cac52fc4c11d05f672f26b452
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-10-01 11:02:17 -0300

    feat: adding Native Base

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar, Text } from "react-native";
+import { StatusBar } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -7,22 +7,26 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { Home } from './src/screens/Home'
 import { Scanner } from './src/screens/Scanner'
 import { Register } from './src/screens/Register'
+import { NativeBaseProvider } from "native-base";
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Scanner" component={Scanner} />
-        <Stack.Screen name="Register" component={Register} />
-      </Stack.Navigator>
+      <NativeBaseProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Scanner" component={Scanner} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </NativeBaseProvider>
     </NavigationContainer>
   );
 }
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text, Button, FlatList, Alert } from 'react-native';
+import { Box, AspectRatio, Image, Stack, HStack, VStack, Text, Heading } from 'native-base';
+
 import firestore from '@react-native-firebase/firestore';
+
+import Img from '../../assets/pilao.jpg';
 
 import { styles } from './styles';
 
@@ -31,6 +34,44 @@ export function Home({ navigation }) {
                 <Text>
                     Lotes cadastrados:
                 </Text>
+
+                <Box
+                    shadow="2"
+                    rounded="lg"
+                    w={{ base: "64", md: "80", lg: "md" }}
+                    _light={{ bg: "coolGray.50" }}
+                    _dark={{ bg: "gray.700" }}
+                >
+                    <AspectRatio w="100%" ratio={ratio}>
+                        <Image source={Img} alt="image base" />
+                    </AspectRatio>
+
+                    <Stack space="2" p="4">
+                        <Text color="gray.400">October 1, 2022</Text>
+                        <Heading size={["md", "lg", "md"]} fontWeight="medium">
+                            Lote de Café Pilão
+                        </Heading>
+                        <Text>
+                            Data de validade: 10/10/2022;
+                            Data de fabricação: 10/10/2018;
+                        </Text>
+
+                        <HStack space="3" px="4" pb="4">
+                            <MoreIcon _light={{ color: "emerald.800" }} _dark={{ color: "emerald.300" }} />
+                            <Box
+                                px="3"
+                                py="2"
+                                mb={["4", "5"]}
+                                bg="primary.400"
+                                rounded="lg"
+                            >
+                                <Text fontWeight="medium" color="white" fontSize="sm">
+                                    CONFERIR VALIDADE
+                                </Text>
+                            </Box>
+                        </HStack>
+                    </Stack>
+                </Box>
 
                 <FlatList
                     horizontal={true}
